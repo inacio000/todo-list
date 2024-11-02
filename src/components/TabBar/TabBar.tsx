c551e548de5fb62ac47471251b4c196777bf0451
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2024-11-02 22:27:48 +0300

    feat: add task count on the tab-bar

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './tabbar.module.scss'
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 const TabBar: React.FC = () => {
+  const tasks = useSelector((state: RootState) => state.todo.tasks);
+
+  const activeTasks = tasks.filter(task => !task.completed && !task.deleted).length;
+  const allTasks = tasks.filter(task => !task.deleted).length;
+
   return (
     <nav className={styles.tabbar}>
       <NavLink to="active" className={({ isActive }) => isActive ? `${styles.active}` : ''}>
-        ТЕКУЩИЕ ДЕЛА
+        ТЕКУЩИЕ ДЕЛА ({activeTasks})
       </NavLink>
       <NavLink to="all" className={({ isActive }) => isActive ? `${styles.active}` : ''}>
-        ВСЕ ДЕЛА
+        ВСЕ ДЕЛА ({allTasks})
       </NavLink>
       <NavLink to="completed" className={({ isActive }) => isActive ? `${styles.active}` : ''}>
         ВЫПОЛНЕННЫЕ ДЕЛА
